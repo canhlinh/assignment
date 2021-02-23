711d77d25de1d308df048dc673aab39498a6eb66
canhlinh <canhlinh.lienson@gmail.com>
2021-02-23 14:05:14 +0700

    Update lab2

--- labs/lab2/main.go
+++ labs/lab2/main.go
@@ -7,6 +7,24 @@ import (
 	"golang.org/x/tour/pic"
 )
 
+func rand(x, y int) uint8 {
+	return uint8((x+y)/2 | x*y | x ^ y)
+}
+
+func Pic(dx, dy int) [][]uint8 {
+
+	pic := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		row := make([]uint8, dy)
+		for y := 0; y < dy; y++ {
+			row[y] = rand(x, y)
+		}
+		pic[x] = row
+	}
+
+	return pic
+}
+
 type Image struct {
 	rec       image.Rectangle
 	colorMode color.Model
@@ -21,19 +39,20 @@ func (i Image) ColorModel() color.Model {
 }
 
 func (i Image) At(x, y int) color.Color {
+	v := rand(x, y)
 	return color.RGBA{
-		0, 0, 255, 255,
+		v, v, 255, 255,
 	}
 }
 
-func NewImage() *Image {
+func NewImage(w, h int) *Image {
 	return &Image{
-		rec:       image.Rect(0, 0, 10, 10),
+		rec:       image.Rect(0, 0, w, h),
 		colorMode: color.RGBAModel,
 	}
 }
 
 func main() {
-	m := NewImage()
+	m := NewImage(10, 10)
 	pic.ShowImage(m)
 }
